50d2dba1787797027b3fa54d579dc8188d917dd8
harsh27-afk <itisharsh27@gmail.com>
2024-12-29 12:36:18 +0530

    user creation code pushed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,1 +1,35 @@
-    
+    import mongoose from "mongoose";
+    import bcrypt from "bcrypt";
+    import jwt from "jsonwebtoken"
+
+    const userSchema = new mongoose.Schema({
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            trim:true,
+            lowercase:true,
+            minLength:[6,"email must be atleast 6 characters long"],
+            maxLength:[50,"email must be atmost 50 characters long"]
+        },
+        password:{
+            type:String,
+            select:false  // avoids password reaching frontend
+        }
+    });
+
+    userSchema.statics.hashPassword = async function(password){
+        return await bcrypt.hash(password,10);
+    } 
+
+    userSchema.methods.isValidPassword = async function(password){
+        return await bcrypt.compare(password,this.password);
+    }
+
+    userSchema.methods.generateJWT=function(){
+        return jwt.sign({email:this.email},process.env.JWT_SECRET );
+    }
+
+    const User=mongoose.model("user",userSchema);
+
+    export default User;
