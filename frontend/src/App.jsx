b7b3bc0fb60693f3725bd5ba364c3df9adcbf051
harsh27-afk <itisharsh27@gmail.com>
2025-01-02 10:15:26 +0530

    context api setup pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes'
+import { UserProvider } from './context/user.context'
 
 const App = () => {
   return (
-    <AppRoutes/>
+    // wrapping AppRoutes in UserProvider helps to provide the state variables in user context  to all the components inside AppRoutes
+    <UserProvider>
+      <AppRoutes/>
+    </UserProvider>
+      
+    
   )
 }
 
