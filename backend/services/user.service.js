3f55877af96c8cc3fd982d14d7d782565f6bea70
harsh27-afk <itisharsh27@gmail.com>
2025-01-03 10:23:45 +0530

    user service updated

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,41 +1,29 @@
-import userModel from "../models/user.model.js"
+import userModel from '../models/user.model.js';
 
 
-export const createUser=async({email,password})=>{
-    if(!email || !password){
-        throw new Error("email and password are required")
+
+export const createUser = async ({
+    email, password
+}) => {
+
+    if (!email || !password) {
+        throw new Error('Email and password are required');
     }
 
-
-    const hashedPassword= await userModel.hashPassword(password)
+    const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userModel.create({
         email,
-        password:hashedPassword
-    })
+        password: hashedPassword
+    });
 
-    return user; 
+    return user;
+
 }
 
-export const loginUser = async ({ email, password }) => {
-    // Check if email and password are provided by the user
-    if (!email || !password) {
-        throw new Error("email and password are required");
-    }
-
-    // Check if user exists in db
-    const user = await userModel.findOne({ email }).select('+password');
-    if (!user) {
-        throw new Error("user not found");
-    }
-
-    // Check if password is correct by comparing the hashed password stored in db
-    const isMatch = await user.isValidPassword(password); // Pass the incoming password here
-    if (!isMatch) {
-        throw new Error("invalid password");
-    }
-
-    // If user exists and password is correct then return the user
-    return user;
-};
-
+export const getAllUsers = async ({ userId }) => {
+    const users = await userModel.find({
+        _id: { $ne: userId }
+    });
+    return users;
+}
