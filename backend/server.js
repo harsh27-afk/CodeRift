a99479f2c19e3c2c4131c14f0078cd8b03c207a8
harsh27-afk <itisharsh27@gmail.com>
2025-01-05 09:01:37 +0530

    socket config methods pushed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import "dotenv/config.js"
 import app from "./app.js"
 import http from "http"
+import {Server} from "socket.io"
 
 const port=process.env.PORT
 
 const server=http.createServer(app);
+const io=new Server(server)
+
+io.on("connection",(socket)=>{
+    console.log("a user connected")
+    
+    socket.on("disconnect",()=>{
+        console.log("user disconnected")
+    })
+})
 
 server.listen(port,()=>{
     console.log(`server listening on port ${port}`);
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/project" element={<Project/>} />
      </Routes>
     
     </BrowserRouter>
