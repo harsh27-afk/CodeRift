72e8168431839dc951cd8de06c740a2ff928e74e
harsh27-afk <itisharsh27@gmail.com>
2025-01-06 11:39:14 +0530

    ai routes and controllers pushed

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,30 @@
-
-import express from "express"
-import morgan from "morgan"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import connect from "./db/db.js"
-import userRoutes from "./routes/user.route.js"
-
-
-const app=express();
+import express from 'express';
+import morgan from 'morgan';
+import connect from './db/db.js';
+import userRoutes from './routes/user.routes.js';
+import projectRoutes from './routes/project.routes.js';
+import aiRoutes from './routes/ai.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 connect();
 
-//
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}))
 
-app.use("/users",userRoutes)
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-    console.log(process.env.MONGODB_URI)
-})
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-export default app;
+app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+app.use("/ai", aiRoutes)
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+export default app; 
